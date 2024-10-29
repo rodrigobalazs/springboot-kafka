5f8c8f99f7d1d3c94868795e3bc74052aefe238c
Rodrigo Balazs <rodrigobalazs@gmail.com>
2024-10-29 13:56:27 -0300

    add initial folders structure + updates pom.xml with Spring Kafka dependencies

---
+++ src/test/java/com/rbalazs/eventsapi/EventsApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rbalazs.eventsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
